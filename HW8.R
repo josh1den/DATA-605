dc95c018c46d26ae28ffa673f74117f900c82d82
Josh Iden <iden.josh@gmail.com>
2022-10-30 17:02:08 -0400

    update git repo

---
+++ HW8.R
@@ -0,0 +1,11 @@
+#11 and #14 on page 303 of probability text
+#1 on page 320-321
+
+#11 A company buys 100 lightbulbs, each of which has an exponential lifetime of
+#1000 hours. What is the expected time for the  rst of these bulbs to burn
+#out? (See Exercise 10.)
+
+14 Assume that X1 and X2 are independent random variables, each having an
+exponential density with parameter  . Show that 
+$Z = X_1 - X_2$ has density
+fZ(z) = (1=2) e􀀀 jzj :
